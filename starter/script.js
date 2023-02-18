d23beb7ae485da827c5bd54c704f958c65e10cb0
Daley Cutler <daley_cutler@Hotmail.co.uk>
2023-02-18 17:17:26 +0000

    added for loop, Prompt user for character set criteria, Prompt user for character set criteria, and other commands

--- starter/script.js
+++ starter/script.js
@@ -1,42 +1,49 @@
-// Array of special characters to be included in password
-var specialCharacters = [  '@',  '%',  '+',  '\\',  '/',  "'",  '!',  '#',  '$',  '^',  '?',  ':',  ',',  ')',  '(',  '}',  '{',  ']',  '[',  '~',  '-',  '_',  '.'];
-
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [  'a',  'b',  'c',  'd',  'e',  'f',  'g',  'h',  'i',  'j',  'k',  'l',  'm',  'n',  'o',  'p',  'q',  'r',  's',  't',  'u',  'v',  'w',  'x',  'y',  'z'];
-
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [  'A',  'B',  'C',  'D',  'E',  'F',  'G',  'H',  'I',  'J',  'K',  'L',  'M',  'N',  'O',  'P',  'Q',  'R',  'S',  'T',  'U',  'V',  'W',  'X',  'Y',  'Z'];
-
-// Function to prompt user for password options
-function getPasswordOptions() {
-  var length = parseInt(
-    prompt('Your password should contain 8 charecters')
-  );
-}
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-}
-
-// Function to generate password with user input
+// Function to generate a password
 function generatePassword() {
+  // Set of characters to choose from based on user criteria
+  const characterSets = {
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    numeric: '0123456789',
+    special: '!@#$%^&*()_+-=[]{}|;:,.<>/?'
+  };
 
+  // Prompt user for password length
+  let passwordLength = parseInt(prompt('How many characters should the password contain?'));
+  while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    passwordLength = parseInt(prompt('Please enter a number between 8 and 128.'));
+  }
+
+  // Prompt user for character set criteria
+  let includeLowercase = confirm('Include lowercase letters?');
+  let includeUppercase = confirm('Include uppercase letters?');
+  let includeNumeric = confirm('Include numeric characters?');
+  let includeSpecial = confirm('Include special characters?');
+
+  // Build character set based on user criteria
+  let characterSet = '';
+  if (includeLowercase) {
+    characterSet += characterSets.lowercase;
+  }
+  if (includeUppercase) {
+    characterSet += characterSets.uppercase;
+  }
+  if (includeNumeric) {
+    characterSet += characterSets.numeric;
+  }
+  if (includeSpecial) {
+    characterSet += characterSets.special;
+  }
+
+  // Generate password by randomly selecting characters from the character set
+  let password = '';
+  for (let i = 0; i < passwordLength; i++) {
+    password += characterSet[Math.floor(Math.random() * characterSet.length)];
+  }
+
+  return password;
 }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+// Call the function to generate a password and log the result to the console
+let password = generatePassword();
+console.log(password);
